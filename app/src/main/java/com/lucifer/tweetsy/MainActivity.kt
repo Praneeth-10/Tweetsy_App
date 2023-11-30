159611fdd7367c91c08dc1e66b4bb732ef47b95b
Praneeth-10 <sp.parasa@gmail.com>
2023-11-29 23:00:25 -0600

    Playing with the composable to fit in the same screen

--- app/src/main/java/com/lucifer/tweetsy/MainActivity.kt
+++ app/src/main/java/com/lucifer/tweetsy/MainActivity.kt
@@ -3,6 +3,13 @@ package com.lucifer.tweetsy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.lucifer.tweetsy.screens.CategoryScreen
 import com.lucifer.tweetsy.screens.DetailScreen
 import com.lucifer.tweetsy.ui.theme.TweetsyTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,7 +24,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             TweetsyTheme {
                 // A surface container using the 'background' color from the theme
-                DetailScreen()
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    CategoryScreen()
+                    Spacer(modifier = Modifier.height(4.dp))
+                    DetailScreen()
+                }
             }
         }
     }
