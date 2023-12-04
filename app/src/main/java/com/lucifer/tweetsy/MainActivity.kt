3070435adea775abf7c47805ceb6eb97b21bbb92
Praneeth-10 <sp.parasa@gmail.com>
2023-12-03 23:30:42 -0600

    Adding TopAppBar via Scaffold() to the main Activity

--- app/src/main/java/com/lucifer/tweetsy/MainActivity.kt
+++ app/src/main/java/com/lucifer/tweetsy/MainActivity.kt
@@ -3,7 +3,16 @@ package com.lucifer.tweetsy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,13 +27,31 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             TweetsyTheme {
                 // A surface container using the 'background' color from the theme
-                App()
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(text = "Tweetsy")
+                            },
+                            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                containerColor = Color.Black,
+                                titleContentColor = Color.White,
+//                                navigationIconContentColor = Color.White
+                            )
+                        )
+                    }
+                ) {
+                    Box(modifier = Modifier.padding(it)) {
+                        App()
+                    }
+                }
             }
         }
     }
@@ -33,17 +60,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val navControl = rememberNavController()
-    NavHost(navController = navControl, startDestination = "categoryScreen"){
-        composable(route = "categoryScreen"){
-            CategoryScreen{
+    NavHost(navController = navControl, startDestination = "categoryScreen") {
+        composable(route = "categoryScreen") {
+            CategoryScreen {
                 navControl.navigate("detailScreen/$it")
             }
         }
         composable(route = "detailScreen/{category}", arguments = listOf(
-            navArgument(name = "category"){
+            navArgument(name = "category") {
                 type = NavType.StringType
             }
-        )){
+        )) {
             DetailScreen()
         }
     }
