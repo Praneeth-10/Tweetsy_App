17dce73e904e0865fe8d4c8ef6dd3387cb65363c
Praneeth-10 <sp.parasa@gmail.com>
2023-12-09 19:34:54 -0600

    removed unnecessary dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,6 @@ dependencies {
     // View Model
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Dagger Hilt
